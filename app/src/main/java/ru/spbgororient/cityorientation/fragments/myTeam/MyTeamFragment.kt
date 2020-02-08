9752201ddfd2993d5ca437a5013323322a3f3666
SprutSDM <zakoylov@gmail.com>
2020-02-09 02:03:13 +0300

    Refactor MyTeamFragment

--- app/src/main/java/ru/spbgororient/cityorientation/fragments/myTeam/MyTeamFragment.kt
+++ app/src/main/java/ru/spbgororient/cityorientation/fragments/myTeam/MyTeamFragment.kt
@@ -1,11 +1,12 @@
 package ru.spbgororient.cityorientation.fragments.myTeam
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.text.InputType
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,101 +15,94 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import kotlinx.android.synthetic.main.fragment_my_team.*
 import ru.spbgororient.cityorientation.R
+import ru.spbgororient.cityorientation.activities.loginActivity.LoginActivity
 import ru.spbgororient.cityorientation.dataController.DataController
-import ru.spbgororient.cityorientation.fragments.listOfQuests.ListOfQuestsFragment
-import ru.spbgororient.cityorientation.network.Network
 
-class MyTeamFragment: Fragment() {
+class MyTeamFragment: Fragment(), MyTeamContract.View {
+    private val presenter: MyTeamContract.Presenter by lazy { MyTeamPresenter(this) }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_my_team, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("MyTeamFragment", "${DataController.instance.team.teamName}")
-        Log.d("MyTeamFragment", "hashCode(): ${this.hashCode()}")
         // TODO: При перелогине в другой аккаунт остаются старые данные.
-        edit_name_team.setText(DataController.instance.team.teamName)
-        edit_login_team.setText(DataController.instance.team.login)
-        edit_password_team.setText(DataController.instance.team.password)
         if (DataController.instance.quests.questId == "")
             button_leave_quest.visibility = View.INVISIBLE
 
-        check_box.setOnCheckedChangeListener { compoundButton, isChecked ->
-            if (isChecked)
-                edit_password_team.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            else
-                edit_password_team.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        check_box.setOnCheckedChangeListener { _, isChecked ->
+            presenter.passwordCheckBoxChanged(isChecked)
+        }
+        button_rename_team.setOnClickListener {
+            presenter.renameTeam(edit_team_name.text.toString())
         }
 
-        button_rename_team.setOnClickListener {
-            if (edit_name_team.text.toString() != "") {
-                DataController.instance.renameTeam(edit_name_team.text.toString(), ::callbackRenameTeam)
-                hideKeyboard()
-            }
-        }
-        edit_name_team.setOnEditorActionListener { v, actionId, event ->
-            when (actionId) {
-                EditorInfo.IME_ACTION_DONE -> {
-                    if (edit_name_team.text.toString() != "")
-                        hideKeyboard()
-                    DataController.instance.renameTeam(edit_name_team.text.toString(), ::callbackRenameTeam)
-                    true
-                }
-                else -> false
+        edit_team_name.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                presenter.renameTeam(edit_team_name.text.toString())
+                true
+            } else {
+                false
             }
         }
         button_leave_team.setOnClickListener {
-            DataController.instance.resetTeam()
-            // TODO
-//            val intent = Intent(context, LoginActivity::class.java)
-//            activity!!.startActivity(intent)
+            presenter.logout()
         }
         button_leave_quest.setOnClickListener {
-            if (DataController.instance.quests.questId != "")
-                DataController.instance.leaveQuest(::callbackLeaveQuest)
+            presenter.leaveQuest()
+        }
+        presenter.onViewCreated()
+    }
+
+    override fun setTeamName(teamName: String) = edit_team_name.setText(teamName)
+
+    override fun setLogin(login: String) = edit_login_team.setText(login)
+
+    override fun setPassword(password: String) = edit_password_team.setText(password)
+
+    override fun showPassword() {
+        edit_password_team.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+    }
+
+    override fun hidePassword() {
+        edit_password_team.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+    }
+
+    override fun hideKeyboard() {
+        activity?.currentFocus?.let { v ->
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(v.windowToken, 0)
         }
     }
 
+    override fun showTeamWasRenamed() = showSnackbar(R.string.team_renamed)
+
+    override fun showLeftQuest() = showSnackbar(R.string.left_quest)
+
+    override fun openLoginActivity() {
+        val intent = Intent(context, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        activity?.startActivity(intent)
+    }
+
+    override fun activateLeaveQuestButton() {
+        button_leave_quest.visibility = View.VISIBLE
+    }
+
+    override fun disableLeaveQuestButton() {
+        button_leave_quest.visibility = View.GONE
+    }
+
+    private fun showSnackbar(@StringRes messageId: Int) {
+        activity?.let { a ->
+            Snackbar.make(a.findViewById(R.id.content_frame), getString(messageId), Snackbar.LENGTH_SHORT).show()
+        }
+    }
 
     fun setVisibleLeaveButton() {
         activity?.runOnUiThread {
             activity?.findViewById<Button>(R.id.button_leave_quest)!!.visibility = View.VISIBLE
-        }
-    }
-
-    /**
-     * Вызывается при завершении запроса на переименование команды.
-     */
-    private fun callbackRenameTeam(response: Network.NetworkResponse) {
-        if (response == Network.NetworkResponse.OK) {
-            activity?.let {
-                Snackbar.make(it.findViewById(R.id.content_frame), getString(R.string.team_renamed),
-                    Snackbar.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    /**
-     * Вызывается при завершении запроса покидание квеста.
-     */
-    private fun callbackLeaveQuest(response: Network.NetworkResponse) {
-        if (response == Network.NetworkResponse.OK) {
-            activity?.runOnUiThread {
-                button_leave_quest.visibility = View.INVISIBLE
-                ListOfQuestsFragment.instance.notifyDataSetChanged()
-                activity?.let {
-                    Snackbar.make(it.findViewById(R.id.content_frame), getString(R.string.left_quest),
-                        Snackbar.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
-
-    private fun hideKeyboard() {
-        activity?.currentFocus?.let { v ->
-            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            imm?.hideSoftInputFromWindow(v.windowToken, 0)
         }
     }
 
---
+++ app/src/main/java/ru/spbgororient/cityorientation/fragments/myTeam/MyTeamPresenter.kt
@@ -0,0 +1,66 @@
+package ru.spbgororient.cityorientation.fragments.myTeam
+
+import ru.spbgororient.cityorientation.dataController.DataController
+import ru.spbgororient.cityorientation.fragments.listOfQuests.ListOfQuestsFragment
+import ru.spbgororient.cityorientation.network.Network
+
+class MyTeamPresenter(private val view: MyTeamContract.View) : MyTeamContract.Presenter {
+
+    override fun onViewCreated() {
+        if (DataController.instance.quests.questId == "") {
+            view.disableLeaveQuestButton()
+        } else {
+            view.activateLeaveQuestButton()
+        }
+        view.setLogin(DataController.instance.team.login)
+        view.setTeamName(DataController.instance.team.teamName)
+        view.setPassword(DataController.instance.team.password)
+    }
+
+    override fun passwordCheckBoxChanged(isChecked: Boolean) {
+        if (isChecked) {
+            view.showPassword()
+        } else {
+            view.hidePassword()
+        }
+    }
+
+    override fun renameTeam(teamName: String) {
+        if (teamName != "") {
+            DataController.instance.renameTeam(teamName, ::callbackRenameTeam)
+            view.hideKeyboard()
+        }
+    }
+
+    override fun logout() {
+        DataController.instance.resetTeam()
+        view.openLoginActivity()
+    }
+
+    override fun leaveQuest() {
+        if (DataController.instance.quests.questId != "") {
+            DataController.instance.leaveQuest(::callbackLeaveQuest)
+        }
+    }
+
+    /**
+     * Вызывается при завершении запроса на переименование команды.
+     */
+    private fun callbackRenameTeam(response: Network.NetworkResponse) {
+        if (response == Network.NetworkResponse.OK) {
+            view.showTeamWasRenamed()
+        }
+    }
+
+    /**
+     * Вызывается при завершении запроса покидание квеста.
+     */
+    private fun callbackLeaveQuest(response: Network.NetworkResponse) {
+        if (response == Network.NetworkResponse.OK) {
+            view.disableLeaveQuestButton()
+            //TODO refactor
+            ListOfQuestsFragment.instance.notifyDataSetChanged()
+            view.showLeftQuest()
+        }
+    }
+}
